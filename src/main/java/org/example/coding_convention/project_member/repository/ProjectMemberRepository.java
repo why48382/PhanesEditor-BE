68fcadad0dc99262ef40fc13b0e79aaaf049b6a3
why4832 <why48382@gmail.com>
2026-07-08 01:09:07 +0900

    refactor: N+1 문제 해결

--- src/main/java/org/example/coding_convention/project_member/repository/ProjectMemberRepository.java
+++ src/main/java/org/example/coding_convention/project_member/repository/ProjectMemberRepository.java
@@ -3,13 +3,18 @@ package org.example.coding_convention.project_member.repository;
 import org.example.coding_convention.project_member.model.ProjectMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ProjectMemberRepository extends JpaRepository<ProjectMember, Integer> {
     List<ProjectMember> findByUser_Idx(Integer userId);
+
     List<ProjectMember> findByProject_Idx(Integer projectId);
 
-    @Query("SELECT p FROM ProjectMember p JOIN FETCH p.project JOIN FETCH p.project.user WHERE p.user.idx = :idx")
-    List<ProjectMember> findByProjectList(Integer idx);
+    @Query("SELECT pm FROM ProjectMember pm " +
+            "JOIN FETCH pm.project p " +
+            "JOIN FETCH p.user " +
+            "WHERE pm.user.idx = :userId")
+    List<ProjectMember> findByProjectList(@Param("userId") Integer userId);
 }
