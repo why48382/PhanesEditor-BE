644bdb463c4f49d8267a6d20ebc0b1cfca05fb10
why4832 <why48382@gmail.com>
2026-07-19 15:03:35 +0900

    refactor: JWT 시크릿 변경과 환경변수 사용하도록 변경

--- src/main/java/org/example/coding_convention/utils/JwtUtil.java
+++ src/main/java/org/example/coding_convention/utils/JwtUtil.java
@@ -4,29 +4,31 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
-import jakarta.servlet.http.Cookie;
+import jakarta.annotation.PostConstruct;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.security.Key;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 public class JwtUtil {
-    private static final String SECRET = "abcdeffghijklmnopqrstuvwxyz0123456";
-    private static final Key KEY = Keys.hmacShaKeyFor(SECRET.getBytes());
-    private static final Long EXP = 1000 * 60 * 240L;
+    @Value("${jwt.secret}")
+    private String secret;
+    private static Key KEY;
+    private static final Long EXP = 1000 * 60 * 60 * 24 * 7L;
+
+    @PostConstruct
+    public void init() {
+        KEY = Keys.hmacShaKeyFor(secret.getBytes());
+    }
 
     public static String generateToken(String email, Integer idx, String nickname) {
 
-        Map<String, String> claims = new HashMap<>();
-        claims.put("idx", "" + idx);
-        claims.put("email", email);
-        claims.put("nickname", nickname);
-        claims.put("role", "USER");
-
         return Jwts.builder()
-                .setClaims(claims)
+                .claim("idx", "" + idx)
+                .claim("email", email)
+                .claim("nickname", nickname)
+                .claim("role", "USER")
                 .setExpiration(new Date(System.currentTimeMillis() + EXP))
                 .signWith(KEY, SignatureAlgorithm.HS256)
                 .compact();
@@ -50,9 +52,7 @@ public class JwtUtil {
 
 
     public static String getValue(Claims claims, String key) {
-        String value = (String) claims.get(key);
-
-        return value;
+        return (String) claims.get(key);
     }
 
     public static Claims getClaims(String token) {
